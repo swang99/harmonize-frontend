f8fe2e03b342a39ef75c0994da258dc13f4a390c
Ben Sheldon <bsheldon02@gmail.com>
2024-04-09 21:34:35 -0400

    Installed ESLint

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('starting up!');
+import $ from 'jquery';
+
+let num = 0;
+$('#main').html(`You've been on this page for ${num} seconds!`);
+setInterval(() => {
+  num += 1;
+  $('#main').html(`You've been on this page for ${num} seconds!`);
+}, 1000);
---
+++ vite.config.js
@@ -0,0 +1,10 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { defineConfig } from 'vite';
+import eslint from 'vite-plugin-eslint';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    eslint(),
+  ],
+});
