7e755405cd98b005be4d9c71f708bfc2077aeada
Ben Sheldon <bsheldon02@gmail.com>
2024-05-14 21:42:23 -0400

    Basic Feed Component, fix store

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,14 @@
 import { create } from 'zustand';
-import { devtools, persist } from 'zustand/middleware';
+import { devtools } from 'zustand/middleware';
 import { immer } from 'zustand/middleware/immer';
 import createPostSlice from './post-slice';
 import createProfileSlice from './profile-slice';
 
 const useStore = create(
-  persist(devtools(immer((...args) => ({
+  devtools(immer((...args) => ({
     postSlice: createPostSlice(...args),
     profileSlice: createProfileSlice(...args),
-  }))), {
-    name: 'user-storage',
-  }),
+  }))),
 );
 
 export default useStore;
