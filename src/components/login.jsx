caeb3d6d38c2172b819ac0646d2adcf7458a0cad
brendanshaw14 <brendanshaw18@gmail.com>
2024-05-15 13:46:22 -0400

    added access token pulling from login, storage of access token and expiration in local

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,8 +1,20 @@
 import { Box, Button, Flex, Heading } from '@chakra-ui/react';
-import React from 'react';
-import redirectToSpotifyAuth from '../utils/SpotifyAuth';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router';
+import { redirectToSpotifyAuth } from '../utils/SpotifyAuth';
 
 const Login = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const accessToken = localStorage.getItem('access_token');
+    const expiresAt = localStorage.getItem('expires_at');
+
+    if (accessToken && new Date().getTime() < expiresAt) {
+      navigate('/home'); // Redirect to home if the token is still valid
+    }
+  }, [navigate]);
+
   return (
     <Box bg="gray.800" minH="100vh" py="20" px="4">
       <Flex direction="column" maxW="md" mx="auto" p="8" bg="gray.700" borderRadius="md">
