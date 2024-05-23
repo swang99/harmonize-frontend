776632545c6ae9a2b85cced36112af9cbdf8daec
brendanshaw14 <brendanshaw18@gmail.com>
2024-05-22 22:18:05 -0400

    player play pause works now

--- src/components/web-player.jsx
+++ src/components/web-player.jsx
@@ -1,5 +1,7 @@
 /* eslint-disable camelcase */
-import { Button, HStack, Image, Spacer, Text, VStack } from '@chakra-ui/react';
+import { Button, Flex, HStack, Image, Spacer, Text, VStack } from '@chakra-ui/react';
+import { faPause, faPlay } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useEffect } from 'react';
 import useStore from '../store';
 import { initializePlayer } from '../utils/spotify-player';
@@ -16,13 +18,9 @@ const SpotifyPlayer = () => {
     initialize();
   }, []);
 
-  useEffect(() => {
-    console.log('Player slice:', playerSlice);
-  }, [playerSlice]);
-
-  useEffect(() => {
-    console.log('Player:', player);
-  }, [player]);
+  const handlePlayPause = async () => {
+    player.togglePlay();
+  };
 
   return (
     playerSlice && (
@@ -34,21 +32,34 @@ const SpotifyPlayer = () => {
       height="10vh"
       bg="gray.100"
       color="white"
-      justifyContent="left"
+      justifyContent="center"
       zIndex="1000"
       padding={5}
     >
-      <Image src={currentTrack && currentTrack.album.images[0].url} alt="Album cover" boxSize="50px" />
-      <VStack align="left" ml={5}>
-        <Text textColor="black" fontWeight="bold">{currentTrack && playerSlice.currentTrack.name}</Text>
-        <Text textColor="gray.500">{currentTrack && playerSlice.currentTrack.artists[0].name}</Text>
-      </VStack>
+      {/* Image and Text Container */}
+      <Flex position="absolute" left="0" height="100%" alignItems="center" pl={5}>
+        <Image src={currentTrack && currentTrack.album.images[0].url} alt="Album cover" boxSize="50px" />
+        <VStack align="left" ml={3} spacing={0}>
+          <Text textColor="black" fontWeight="bold">{currentTrack && playerSlice.currentTrack.name}</Text>
+          <Text textColor="gray.500">{currentTrack && playerSlice.currentTrack.artists[0].name}</Text>
+        </VStack>
+      </Flex>
+
+      {/* Centered Play/Pause Button */}
       <Spacer />
-        {playerSlice && playerSlice.activated && (
-        <Button onClick={null} ml={4}>
-          Play/Pause
+      {playerSlice && playerSlice.activated && (
+        <Button
+          onClick={handlePlayPause}
+          borderRadius="full"
+          bg="gray.200"
+          mx="auto"
+          w="50px"
+          h="50px"
+        >
+          <FontAwesomeIcon icon={playerSlice.paused ? faPlay : faPause} />
         </Button>
-        )}
+      )}
+      <Spacer />
     </HStack>
     )
   );
