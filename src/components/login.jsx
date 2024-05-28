530c0e7609d30dfd98c6afa7d95207eaab518adb
Emily Gao <emilygao@Emilys-MacBook-Pro.local>
2024-05-28 17:49:41 -0400

    added styling

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-import { Box, Button, Heading, VStack } from '@chakra-ui/react';
+import { Box, Button, Heading, VStack, Image, Text } from '@chakra-ui/react';
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router';
 import { redirectToSpotifyAuth } from '../utils/SpotifyAuth';
@@ -17,10 +17,38 @@ const Login = () => {
 
   return (
     <Box bg="teal.800" w="100vw" h="100vh" align="center">
-      <VStack p="5%" w="80%" align="center" justify="center" spacing={5}>
-        <Heading as="h1" fontSize="96px" color="white">Harmonize.</Heading>
-        <Heading as="h2" size="xl" color="gray.200">Spotify sharing made easy-- so new music is always at your fingertips.</Heading>
-        <Button colorScheme="teal" fontSize="24px" borderRadius="full" w="300px" h="50" variant="solid" className="login-button" mt={8} onClick={redirectToSpotifyAuth}>Login with Spotify</Button>
+      <Box bg="teal.800"
+        position="absolute"
+        top="0%" /* Adjust to set the position 20% down the screen */
+        left="50%"
+        transform="translate(-50%, -50%)"
+        w="100vw"
+      >
+        <Image
+          mt="60vh"
+          src="/src/img/logoWhite.png"
+          alt="logo"
+          width="40vw"
+          height="auto"
+        />
+      </Box>
+      <VStack p="5%" w="90%" align="center" justify="center" spacing={5} mt="50vh">
+        <Text fontsize="50px" color="gray.200">Spotify sharing made easy - new music always at your fingertips.</Text>
+        <Button
+          colorScheme="teal"
+          color="white"
+          _hover={{ bg: 'teal.600', color: 'white' }}
+          fontSize="24px"
+          borderRadius="full"
+          w="300px"
+          h="50"
+          variant="outline"
+          className="login-button"
+          mt={8}
+          onClick={redirectToSpotifyAuth}
+        >
+          Login with Spotify
+        </Button>
       </VStack>
     </Box>
   );
